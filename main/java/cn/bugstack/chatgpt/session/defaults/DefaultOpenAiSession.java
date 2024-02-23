43284a439dd2be33cac3c1ea8c7033ac3f3bef72
2019212576 <q1332641044@gmail.com>
2024-02-23 15:03:13 +0800

    流式应答会话设计实现

--- main/java/cn/bugstack/chatgpt/session/defaults/DefaultOpenAiSession.java
+++ main/java/cn/bugstack/chatgpt/session/defaults/DefaultOpenAiSession.java
@@ -5,8 +5,17 @@ import cn.bugstack.chatgpt.domain.chat.ChatCompletionRequest;
 import cn.bugstack.chatgpt.domain.chat.ChatCompletionResponse;
 import cn.bugstack.chatgpt.domain.qa.QACompletionRequest;
 import cn.bugstack.chatgpt.domain.qa.QACompletionResponse;
+import cn.bugstack.chatgpt.session.Configuration;
 import cn.bugstack.chatgpt.session.OpenAiSession;
+import cn.hutool.http.ContentType;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.reactivex.Single;
+import okhttp3.MediaType;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.sse.EventSource;
+import okhttp3.sse.EventSourceListener;
 
 /**
  * @description:
@@ -16,10 +25,17 @@ import io.reactivex.Single;
  */
 public class DefaultOpenAiSession implements OpenAiSession {
 
-    private IOpenAiApi openAiApi;
+    /** 配置信息 */
+    private final Configuration configuration;
+    /** OpenAI 接口 */
+    private final IOpenAiApi openAiApi;
+    /** 工厂事件 */
+    private final EventSource.Factory factory;
 
-    public DefaultOpenAiSession(IOpenAiApi openAiApi) {
-        this.openAiApi = openAiApi;
+    public DefaultOpenAiSession(Configuration configuration) {
+        this.configuration = configuration;
+        this.openAiApi = configuration.getOpenAiApi();
+        this.factory = configuration.createRequestFactory();
     }
 
     @Override
@@ -27,6 +43,20 @@ public class DefaultOpenAiSession implements OpenAiSession {
         return this.openAiApi.completions(qaCompletionRequest).blockingGet();
     }
 
+    @Override
+    public EventSource completions(QACompletionRequest qaCompletionRequest, EventSourceListener eventSourceListener) throws JsonProcessingException{
+        // 核心参数校验；不对用户的传参做更改，只返回错误信息。
+        if(!qaCompletionRequest.isStream()){
+            throw new RuntimeException("illegal parameter stream is false!");
+        }
+
+        // 构建请求信息
+        Request request = new Request.Builder()
+                .url(configuration.getApiHost().concat(IOpenAiApi.v1_completions))
+                .post(RequestBody.create(MediaType.parse(ContentType.JSON.getValue()), new ObjectMapper().writeValueAsString(qaCompletionRequest)))
+                .build();
+        return factory.newEventSource(request,eventSourceListener);
+    }
     @Override
     public QACompletionResponse completions(String question) {
         QACompletionRequest request = QACompletionRequest
@@ -40,5 +70,24 @@ public class DefaultOpenAiSession implements OpenAiSession {
     @Override
     public ChatCompletionResponse completions(ChatCompletionRequest chatCompletionRequest) {
         return this.openAiApi.completions(chatCompletionRequest).blockingGet();
+    }
+
+    @Override
+    public EventSource chatCompletions(ChatCompletionRequest chatCompletionRequest, EventSourceListener eventSourceListener) throws JsonProcessingException {
+        // 核心参数校验；不对用户的传参做更改，只返回错误信息。
+        if (!chatCompletionRequest.isStream()) {
+            throw new RuntimeException("illegal parameter stream is false!");
+        }
+
+        // 构建请求信息
+        Request request = new Request.Builder()
+                // url: https://api.openai.com/v1/chat/completions - 通过 IOpenAiApi 配置的 POST 接口，用这样的方式从统一的地方获取配置信息
+                .url(configuration.getApiHost().concat(IOpenAiApi.v1_chat_completions))
+                // 封装请求参数信息，如果使用了 Fastjson 也可以替换 ObjectMapper 转换对象
+                .post(RequestBody.create(MediaType.parse(ContentType.JSON.getValue()), new ObjectMapper().writeValueAsString(chatCompletionRequest)))
+                .build();
+
+        // 返回结果信息；EventSource 对象可以取消应答
+        return factory.newEventSource(request, eventSourceListener);
     }
 }
